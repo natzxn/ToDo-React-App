95cdc0ef72575382ffc165d3b2fd2b0251457727
Natalia <114617409+natzxn@users.noreply.github.com>
2024-02-09 22:11:12 +0100

    import types from other file

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,23 +1,20 @@
 import { useState, useEffect } from 'react';
 import firebase from '../firebase';
 import { getFirestore, collection, onSnapshot } from 'firebase/firestore';
-
-interface Todo {
-  id: string;
-}
+import { TodoItem } from '../components/Todos'; 
 
 //useTodos collects todos from Firebase cloud
-export function useTodos(): Todo[] {
-  const [todos, setTodos] = useState<Todo[]>([]);
+export function useTodos(): TodoItem[] {
+  const [todos, setTodos] = useState<TodoItem[]>([]);
 
   useEffect(() => {
     const db = collection(getFirestore(firebase), 'todos');
 
     const unsubscribe = onSnapshot(db, (snapshot) => {
-      const data: Todo[] = snapshot.docs.map((doc) => ({
+      const data: TodoItem[] = snapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
-      }));
+      } as TodoItem)); //doc.data() as TodoItem
       setTodos(data);
     });
 
