adab7d2d67fb259cbcf1ff1f3ddc1f47890198e7
Natalia <114617409+natzxn@users.noreply.github.com>
2024-03-07 22:46:51 +0100

    changes to the component DaysView.tsx

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,7 +1,6 @@
 import React, { FC, useContext } from 'react';
 import Todo from './Todo';
-import Next7Days from './Next7Days';
-import Prev7Days from './Prev7Days';
+import DaysView from './DaysView';
 import { TodoContext, TodoContextProps } from '../context';
 import moment from 'moment';
 
@@ -24,54 +23,26 @@ const Todos: FC = () => {
   }
 
   const { todos, selectedDay }: TodoContextProps = contextValue;
-  const today = parseInt(moment().format('d'), 10); 
 
-    return (
-      <div className="Todos">
-        <div className="selected-task">{selectedDay}</div>
-        <div className="todos">
-          {selectedDay === "next 7 days" ? (
-            <Next7Days
-              todos={todos.filter((todo: TodoItem) => { 
-                const tomorrow = moment().add(1, "day").startOf("day"); 
-                const next7DaysWithoutToday = moment(tomorrow).add(7, "days"); 
-                const todoDate = moment(todo.date, "DD/MM/YYYY");
-                return todoDate.isBetween( 
-                  tomorrow,
-                  next7DaysWithoutToday,
-                  undefined,
-                  "[]"
-                ); //FILTER NEXT 7 DAYS (WITHOUT 'TODAY')
-              })}
-            />
-          ) : selectedDay === "previous 7 days" ? (
-            <Prev7Days
-              todos={todos.filter((todo: TodoItem) => {
-                const today = moment().startOf("day");
-                const previous7Days = moment()
-                  .subtract(7, "days")
-                  .startOf("day"); 
-                const todoDate = moment(todo.date, "DD/MM/YYYY"); 
-
-                return (
-                  todoDate.isBetween(previous7Days, today, undefined, "(]") ||
-                  todoDate.isSame(today, "day") 
-                );
-              })}
-            />
-          ) : selectedDay === "all days" ? (
-            todos.map((todo: TodoItem) => <Todo todo={todo} key={todo.id} />) 
-          ) : (
-            todos.map((todo: TodoItem) =>
-              selectedDay === "today" &&
-              moment(todo.date, "DD/MM/YYYY").isSame(moment(), "day") ? ( 
-                <Todo todo={todo} key={todo.id} />
-              ) : null
-            )
-          )}
-        </div>
+  return (
+    <div className="Todos">
+      <div className="selected-task">{selectedDay}</div>
+      <div className="todos">
+        {selectedDay === 'next 7 days' || selectedDay === 'previous 7 days' ? (
+          <DaysView todos={todos} viewType={selectedDay === 'next 7 days' ? 'next' : 'previous'} />
+        ) : selectedDay === 'all days' ? (
+          todos.map((todo: TodoItem) => <Todo todo={todo} key={todo.id} />)
+        ) : (
+          todos.map((todo: TodoItem) =>
+            selectedDay === 'today' &&
+            moment(todo.date, 'DD/MM/YYYY').isSame(moment(), 'day') ? (
+              <Todo todo={todo} key={todo.id} />
+            ) : null
+          )
+        )}
       </div>
-    ); //FILTER TODAY TODOS
-}
+    </div>
+  );
+};
 
 export default Todos;
