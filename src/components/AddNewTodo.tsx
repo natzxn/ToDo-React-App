89e386c5019548bb9511025942067383b832b317
Natalia <114617409+natzxn@users.noreply.github.com>
2024-03-10 23:01:20 +0100

    add TodoForm features

--- src/components/AddNewTodo.tsx
+++ src/components/AddNewTodo.tsx
@@ -1,74 +1,144 @@
+// AddNewTodo.tsx
 import React, { useState } from 'react';
 import Modal from './Modal';
-import TodoForm from './TodoForm';
-import firebase from '../firebase'
-import { getFirestore, collection, addDoc } from 'firebase/firestore'
+import firebase from '../firebase';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
 import moment from 'moment';
-
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { DatePicker, TimePicker } from '@mui/x-date-pickers';
+import 'moment/locale/en-gb';
 
 interface AddNewTodoProps {}
 
 const AddNewTodo: React.FC<AddNewTodoProps> = () => {
+  const [showModal, setShowModal] = useState<boolean>(() => false);
+  const [formState, setFormState] = useState({
+    text: '',
+    day: new Date(),
+    time: new Date(),
+  });
 
-  //STATE
-    const [showModal, setShowModal] = useState<boolean>(false);
-    const [text, setText] = useState<string>('');
-    const [day, setDay] = useState(new Date())
-    const [time, setTime] = useState(new Date())
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
 
+    if (formState.text) {
+      const db = collection(getFirestore(firebase), 'todos');
 
-    //HANDLE SUMBIT
-  async  function handleSubmit(e:React.FormEvent<HTMLFormElement>){
-        e.preventDefault(); //PREVENT FROM REFRESH WHEN SUBMITTING
+      try {
+        const docRef = await addDoc(db, {
+          text: formState.text,
+          date: moment(formState.day).valueOf(),
+          day: moment(formState.day).format('d'),
+          time: moment(formState.time).valueOf(),
+          checked: false,
+          color: 'black',
+        });
 
-        if (text) {
-            const db = collection(getFirestore(firebase), "todos");
-      
-            try {
-              const docRef = await addDoc(db, {
-                text: text,
-                date: moment(day).format('DD/MM/YYYY'),
-                day: moment(day).format('d'),
-                time: moment(time).format('HH:mm'),
-                checked: false,
-                color: 'black',
-              });
-      
-              console.log('Document written with ID: ', docRef.id);
-            } catch (error) {
-              console.error('Error adding document: ', error);
-            }
+        console.log('Document written with ID: ', docRef.id);
+      } catch (error) {
+        console.error('Error adding document: ', error);
+      }
 
-                setShowModal(false)
-                setText('')
-                setDay(new Date())
-                setTime(new Date())
-        }
+      setShowModal(false);
+      setFormState({
+        text: '',
+        day: new Date(),
+        time: new Date(),
+      });
     }
+  }
 
-    return (
-        <div className='AddNewTodo'>
-            <div className="btn">
-                <button onClick={() => setShowModal(true)}>
-                <img width="19" height="19" src="https://img.icons8.com/ios-glyphs/19/FFFFFF/plus-math.png" alt="plus-math"/>New Todo
-                </button>
-            </div>
-            <Modal showModal={showModal} setShowModal={setShowModal}>
-                <TodoForm 
-                handleSubmit={handleSubmit}
-                heading = 'Add new to do!'
-                text={text}
-                setText={setText}
-                day={day}
-                setDay={setDay}
-                time={time}
-                setTime={setTime}
-                showButtons={true}
-                setShowModal={setShowModal} 
+  return (
+    <div className="AddNewTodo">
+      <div className="btn">
+        <button onClick={() => setShowModal(true)}>
+          <img
+            width="19"
+            height="19"
+            src="https://img.icons8.com/ios-glyphs/19/FFFFFF/plus-math.png"
+            alt="plus-math"
+          />
+          New Todo
+        </button>
+      </div>
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <div className="TodoForm">
+          <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale="en-gb">
+            <form onSubmit={handleSubmit}>
+              <div className="text">
+                <input
+                  type="text"
+                  value={formState.text}
+                  onChange={(e) => {
+                    const inputValue = e.target.value;
+                    if (inputValue.trim() !== "" || inputValue === "") {
+                      setFormState((prev) => ({ ...prev, text: inputValue }));
+                    }
+                  }}
+                  placeholder="To do..."
+                  autoFocus
+                  maxLength={45}
+                  required
                 />
-            </Modal>
+              </div>
+              <div className="pick-day">
+                <div className="title">
+                  <img
+                    width="35"
+                    height="35"
+                    src="https://img.icons8.com/fluency/35/calendar--v1.png"
+                    alt="calendar--v1"
+                  />
+                  <p>Choose a day</p>
+                </div>
+                <DatePicker
+                  value={moment(formState.day)}
+                  onChange={(newDay) => newDay && setFormState((prev) => ({ ...prev, day: newDay.toDate() }))}
+                />
+              </div>
+              <div className="pick-time">
+                <div className="title">
+                  <img
+                    width="35"
+                    height="35"
+                    src="https://img.icons8.com/fluency/35/clock--v1.png"
+                    alt="clock--v1"
+                  />
+                  <p>Choose time</p>
+                </div>
+                <TimePicker
+                  value={moment(formState.time)}
+                  onChange={(newTime) => newTime && setFormState((prev) => ({ ...prev, time: newTime.toDate() }))}
+                />
+              </div>
+              <div className="buttons">
+                <div className="cancel" onClick={() => setShowModal(false)}>
+                  <img
+                    width="22"
+                    height="22"
+                    src="https://img.icons8.com/fluency-systems-regular/22/multiply.png"
+                    alt="multiply"
+                  />
+                </div>
+                <div className="confirm">
+                  <button type="submit">
+                    <img
+                      width="19"
+                      height="19"
+                      src="https://img.icons8.com/ios-glyphs/19/FFFFFF/plus-math.png"
+                      alt="plus-math"
+                    />{' '}
+                    Add task
+                  </button>
+                </div>
+              </div>
+            </form>
+          </LocalizationProvider>
         </div>
-    );
-}
+      </Modal>
+    </div>
+  );
+};
 
-export default AddNewTodo
+export default AddNewTodo;
