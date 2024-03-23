1f30018b0082b565dd75fe082a0116c8f94abf8d
Natalia <114617409+natzxn@users.noreply.github.com>
2024-03-24 00:44:47 +0100

    add userID

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -8,11 +8,12 @@ import moment from 'moment';
 interface TodoItem {
     id: string;
     text: string;
-    time: string;
-    date: string;
+    time: number;
+    date: number;
     day: string;
     checked: boolean;
     color: string;
+    userId: string;
 }
 
 //CONTEXT
@@ -24,16 +25,24 @@ const Todos: FC = () => {
 
   const { todos, selectedDay }: TodoContextProps = contextValue;
 
+  const filteredTodos = todos.filter((todo: TodoItem) => {
+    if (!contextValue?.user) {
+      return [];
+    }
+    return todo.userId === contextValue.user.uid;
+  });
+    
+
   return (
     <div className="Todos">
       <div className="selected-task">{selectedDay}</div>
       <div className="todos">
         {selectedDay === 'next 7 days' || selectedDay === 'previous 7 days' ? (
-          <DaysView todos={todos} viewType={selectedDay === 'next 7 days' ? 'next' : 'previous'} />
+          <DaysView todos={filteredTodos} viewType={selectedDay === 'next 7 days' ? 'next' : 'previous'} />
         ) : selectedDay === 'all days' ? (
-          todos.map((todo: TodoItem) => <Todo todo={todo} key={todo.id} />)
+          filteredTodos.map((todo: TodoItem) => <Todo todo={todo} key={todo.id} />)
         ) : (
-          todos.map((todo: TodoItem) =>
+          filteredTodos.map((todo: TodoItem) =>
             selectedDay === 'today' &&
             moment(todo.date).isSame(moment(), 'day') ? (
               <Todo todo={todo} key={todo.id} />
