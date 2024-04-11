422f7fe7d60f4cebf73142d510435e575f3d9509
Natalia <114617409+natzxn@users.noreply.github.com>
2024-04-11 18:44:19 +0200

    add css module styles

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { auth } from '../firebase';
 import { useNavigate } from 'react-router-dom';
+import styles from '../styles/Login.module.css'
 
 const LoginPage = () => {
   const navigate = useNavigate(); 
@@ -22,9 +23,11 @@ const LoginPage = () => {
   };
 
   return (
-    <div className='LoginPage'>
-      <div className='box'>
-        <button className='login' onClick={handleLogin}><img src='https://cdn.iconscout.com/icon/free/png-256/free-google-152-189813.png' width="30"></img> Login with Google</button>
+    <div className={styles.LoginPage}>
+      <div className={styles.box}>
+        <div className={styles.btn}>
+        <button className={styles.login} onClick={handleLogin}><img src='https://cdn.iconscout.com/icon/free/png-256/free-google-152-189813.png' width="30" alt="login"></img> Login with Google</button>
+        </div>
       </div>
     </div>
   );
