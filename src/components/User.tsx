939bad45f4add5bc15c306291cfda89ac7cd1c1d
Natalia <114617409+natzxn@users.noreply.github.com>
2024-04-11 17:53:21 +0200

    add style css module

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import logo from '../images/logo.png';
 import Logout from './Logout';
+import styles from '../styles/User.module.css'
 
 function User(){
 //Logout imitation*
     return (
-        <div className='User'>
-            <div className="logo">
+        <div className={styles.User}>
+            <div className={styles.logo}>
                 <img src={logo} alt="logo" />
             </div>
-            <div className="info">
+            <div className={styles.info}>
                 <p>Your to-do list!</p>
                 <Logout />
             </div>
