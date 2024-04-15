da19674b813db16c9047df7b1464e9b9092fe535
Natalia <114617409+natzxn@users.noreply.github.com>
2024-04-15 13:44:07 +0200

    fix security issue with storing todos

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,25 +1,32 @@
 import { useState, useEffect } from 'react';
 import firebase from '../firebase';
-import { getFirestore, collection, onSnapshot } from 'firebase/firestore';
+import { getFirestore, collection, onSnapshot, query, where } from 'firebase/firestore';
 import { TodoItem } from '../components/Todos'; 
+import { getAuth } from 'firebase/auth';
 
 //useTodos collects todos from Firebase cloud
 export function useTodos(): TodoItem[] {
   const [todos, setTodos] = useState<TodoItem[]>([]);
+  const auth = getAuth();
+  const currentUser = auth.currentUser;
 
   useEffect(() => {
-    const db = collection(getFirestore(firebase), 'todos');
+    if (!currentUser) return; 
 
-    const unsubscribe = onSnapshot(db, (snapshot) => {
+    const db = getFirestore(firebase);
+    const todosCollection = collection(db, 'todos');
+    const userTodosQuery = query(todosCollection, where('userId', '==', currentUser.uid));
+
+    const unsubscribe = onSnapshot(userTodosQuery, (snapshot) => {
       const data: TodoItem[] = snapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
-      } as TodoItem)); //doc.data() as TodoItem
+      } as TodoItem));
       setTodos(data);
     });
 
     return () => unsubscribe()
-  }, []); //empty array as second argument to render effect only once
+  }, [currentUser]); 
 
   return todos;
 }
