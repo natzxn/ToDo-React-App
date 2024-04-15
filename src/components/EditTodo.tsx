38a0e7d887d45bd9b2abe94ef8a183cc5403d820
Natalia <114617409+natzxn@users.noreply.github.com>
2024-04-15 12:56:22 +0200

    add ui error information to invalid date and time

--- src/components/EditTodo.tsx
+++ src/components/EditTodo.tsx
@@ -23,7 +23,7 @@ function EditTodo(): JSX.Element {
   // EDIT TEXT DAY AND TIME
   useEffect(() => {
     setText(selectedTodo?.text ?? "");
-    setDay(moment(selectedTodo?.date).toDate() ?? new Date());
+    setDay(moment(selectedTodo?.date).toDate() ?? new Date()); 
     setTime(moment(selectedTodo?.time).toDate() ?? new Date());
   }, [selectedTodo]);
   
@@ -32,6 +32,12 @@ function EditTodo(): JSX.Element {
   // UPDATE THE DATA WHEN EDITING TODO
   const handleEditSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    if (!moment(day).isValid() || !moment(time).isValid()) {
+      console.error('Invalid date or time');
+      return;
+    }
+    
     const db = getFirestore(firebase);
     const todoRef = doc(db, "todos", selectedTodo!.id);
     updateDoc(todoRef, {
@@ -52,86 +58,95 @@ function EditTodo(): JSX.Element {
 
   return (
     <>
-    {selectedTodo ? (
-    <div className={styles.EditTodo}>
-      <div className={styles.header}>
-        <img
-          width="25"
-          height="25"
-          src="https://img.icons8.com/office/25/pencil-tip.png"
-          alt="pencil-tip"
-        />{" "}
-        Edit Todo
-      </div>
-      <div className="container">
-        <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale="en-gb">
-          <form onSubmit={handleEditSubmit} className={todoform.TodoForm}>
-            <div className={todoform.text}>
-              <input
-                type="text"
-                value={text}
-                onChange={(e) => {
-                  const inputValue = e.target.value;
-                  if (inputValue.trim() !== "" || inputValue === "") {
-                    setText(inputValue);
-                  }
-                }}
-                placeholder="Edit todo..."
-                autoFocus
-                maxLength={45}
-                required
-              />
-            </div>
-            <div className={todoform.pickday}>
-              <div className={todoform.title}>
-                <img
-                  width="35"
-                  height="35"
-                  src="https://img.icons8.com/fluency/35/calendar--v1.png"
-                  alt="calendar--v1"
-                />
-                <p>Choose a day</p>
-              </div>
-              <DatePicker
-                value={moment(day)}
-                onChange={(newDay) => newDay && setDay(newDay.toDate())}
-              />
-            </div>
-            <div className={todoform.picktime}>
-              <div className={todoform.title}>
-                <img
-                  width="35"
-                  height="35"
-                  src="https://img.icons8.com/fluency/35/clock--v1.png"
-                  alt="clock--v1"
-                />
-                <p>Choose time</p>
-              </div>
-              <TimePicker
-                value={moment(time)}
-                onChange={(newTime) =>
-                  newTime && setTime(moment(newTime).toDate())
-                }
-              />
-            </div>
-            <div className={todoform.buttons}>
-              <div className={todoform.confirm}>
-                <button type={todoform.submit}>Confirm</button>
-              </div>
-              <div className={todoform.cancel} onClick={handleCloseEdit}>
-                <img
-                  width="22"
-                  height="22"
-                  src="https://img.icons8.com/fluency-systems-regular/22/multiply.png"
-                  alt="multiply"
-                />
-              </div>
-            </div>
-          </form>
-        </LocalizationProvider>
-      </div>
-    </div>
-    ) : null}
+      {selectedTodo ? (
+        <div className={styles.EditTodo}>
+          <div className={styles.header}>
+            <img
+              width="25"
+              height="25"
+              src="https://img.icons8.com/office/25/pencil-tip.png"
+              alt="pencil-tip"
+            />{" "}
+            Edit Todo
+          </div>
+          <div className="container">
+            <LocalizationProvider
+              dateAdapter={AdapterMoment}
+              adapterLocale="en-gb"
+            >
+              <form onSubmit={handleEditSubmit} className={todoform.TodoForm}>
+                <div className={todoform.text}>
+                  <input
+                    type="text"
+                    value={text}
+                    onChange={(e) => {
+                      const inputValue = e.target.value;
+                      if (inputValue.trim() !== "" || inputValue === "") {
+                        setText(inputValue);
+                      }
+                    }}
+                    placeholder="Edit todo..."
+                    autoFocus
+                    maxLength={45}
+                    required
+                  />
+                </div>
+                <div className={todoform.pickday}>
+                  <div className={todoform.title}>
+                    <img
+                      width="35"
+                      height="35"
+                      src="https://img.icons8.com/fluency/35/calendar--v1.png"
+                      alt="calendar--v1"
+                    />
+                    <p>Choose a day</p>
+                  </div>
+                  <DatePicker
+                    value={moment(day)}
+                    onChange={(newDay) => newDay && setDay(newDay.toDate())}
+                  />
+                  {!moment(day).isValid() && (
+                    <div className={styles.error}>Invalid date</div>
+                  )}
+                </div>
+                <div className={todoform.picktime}>
+                  <div className={todoform.title}>
+                    <img
+                      width="35"
+                      height="35"
+                      src="https://img.icons8.com/fluency/35/clock--v1.png"
+                      alt="clock--v1"
+                    />
+                    <p>Choose time</p>
+                  </div>
+                  <TimePicker
+                    value={moment(time)}
+                    onChange={(newTime) =>
+                      newTime && setTime(moment(newTime).toDate())
+                    }
+                  />
+                  {!moment(time).isValid() && (
+                    <div className={styles.error}>Invalid time</div>
+                  )}
+                </div>
+                <div className={todoform.buttons}>
+                  <div className={todoform.confirm}>
+                    <button type={todoform.submit}>Confirm</button>
+                  </div>
+                  <div className={todoform.cancel} onClick={handleCloseEdit}>
+                    <img
+                      width="22"
+                      height="22"
+                      src="https://img.icons8.com/fluency-systems-regular/22/multiply.png"
+                      alt="multiply"
+                    />
+                  </div>
+                </div>
+              </form>
+            </LocalizationProvider>
+          </div>
+        </div>
+      ) : null}
     </>
   );
 }
